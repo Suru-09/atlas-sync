d1fe59fb846199a0fde9710a687106f6d6298c32
Suru-09 <dragos.surugiu13@yahoo.com>
2025-05-02 21:32:15 +0300

    Interesting example

--- atlas-sync/src/main.rs
+++ atlas-sync/src/main.rs
@@ -6,11 +6,159 @@ mod item;
 mod uuid_wrapper;
 mod watcher;
 
-use crdt::crdt::*;
-use std::path::Path;
+use libp2p::{
+    core::upgrade,
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    futures::StreamExt,
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    mplex,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmBuilder},
+    tcp::TokioTcpConfig,
+    NetworkBehaviour, PeerId, Transport,
+};
+use log::{error, info};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
+
 use watcher::watcher::watch_path;
 
-fn main() -> notify::Result<()> {
-    watch_path(Path::new("src/resources/"))?;
-    Ok(())
+const WATCHED_FILE_PATH: &str = "src/resources/test_watcher";
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("watcher"));
+
+type MyFiles = Vec<MyFile>;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyFile {
+    name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FilesResponse {
+    data: MyFiles,
+    receiver: String,
+}
+
+enum EventType {
+    FileUploaded(MyFile),
+    AllFiles(FilesResponse),
+}
+
+#[derive(NetworkBehaviour)]
+struct MyFileBehavior {
+    floodsub: Floodsub,
+    mdns: Mdns,
+    #[behaviour(ignore)]
+    response_sender: mpsc::UnboundedSender<FilesResponse>,
+}
+
+impl NetworkBehaviourEventProcess<FloodsubEvent> for MyFileBehavior {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        match event {
+            FloodsubEvent::Message(msg) => {
+                if let Ok(resp) = serde_json::from_slice::<FilesResponse>(&msg.data) {
+                    if resp.receiver == PEER_ID.to_string() {
+                        info!("Response from {}:", msg.source);
+                        resp.data.iter().for_each(|r| info!("{:?}", r));
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+impl NetworkBehaviourEventProcess<MdnsEvent> for MyFileBehavior {
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(discovered_list) => {
+                for (peer, _addr) in discovered_list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    error!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS)
+        .expect("can create auth keys");
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    let mut behaviour = MyFileBehavior {
+        floodsub: Floodsub::new(PEER_ID.clone()),
+        mdns: Mdns::new(Default::default())
+            .await
+            .expect("can create mdns"),
+        response_sender,
+    };
+
+    behaviour.floodsub.subscribe(TOPIC.clone());
+
+    let mut swarm = SwarmBuilder::new(transp, behaviour, PEER_ID.clone())
+        .executor(Box::new(|fut| {
+            tokio::spawn(fut);
+        }))
+        .build();
+
+    Swarm::listen_on(
+        &mut swarm,
+        "/ip4/0.0.0.0/tcp/0"
+            .parse()
+            .expect("can get a local socket"),
+    )
+    .expect("swarm can be started");
+
+    let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+
+    loop {
+        let evt = {
+            tokio::select! {
+                _ = stdin.next_line() => Some(EventType::FileUploaded(MyFile { name: String::from("Hello") })),
+                event = swarm.next() => {
+                    info!("Unhandled Swarm Event: {:?}", event);
+                    None
+                },
+                response = response_rcv.recv() => Some(EventType::AllFiles(response.expect("response exists"))),
+            }
+        };
+
+        if let Some(event) = evt {
+            match event {
+                EventType::FileUploaded(resp) => {
+                    info!("File uploaded!");
+                }
+                EventType::AllFiles(line) => {
+                    info!("All files command");
+                }
+            }
+        }
+    }
 }
--- atlas-sync/src/watcher.rs
+++ atlas-sync/src/watcher.rs
@@ -2,15 +2,19 @@ pub mod watcher {
     use notify::{recommended_watcher, Event, RecursiveMode, Watcher};
     use std::path::Path;
     use std::sync::mpsc;
+    use tokio::sync::mpsc::Sender;
 
-    pub fn watch_path(path: &Path) -> notify::Result<()> {
+    pub fn watch_path(path: &Path, file_tx: Sender<String>) -> notify::Result<()> {
         let (tx, rx) = mpsc::channel::<notify::Result<Event>>();
         let mut watcher = recommended_watcher(tx)?;
 
         watcher.watch(path, RecursiveMode::Recursive)?;
         for res in rx {
             match res {
-                Ok(event) => println!("event: {:?}", event.kind),
+                Ok(event) => {
+                    let _ = file_tx.send(format!("{event:?}"));
+                    println!("event: {:?}", event.kind);
+                }
                 Err(e) => println!("watch error: {:?}", e),
             }
         }
